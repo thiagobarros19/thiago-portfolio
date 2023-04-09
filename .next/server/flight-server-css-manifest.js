6138d4ed03f4e315c92d51af3868b9facde5ffb1
Thiago Barros <thiagobarros@MacBook-Air-de-Thiago.local>
2023-04-08 22:42:53 -0300

    initial commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/thiagobarros/projects/thiago-portfolio/app/page.tsx": [
+      "/Users/thiagobarros/projects/thiago-portfolio/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/thiagobarros/projects/thiago-portfolio/app/layout.tsx": [
+      "/Users/thiagobarros/projects/thiago-portfolio/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/thiagobarros/projects/thiago-portfolio/app/page": [
+      "/Users/thiagobarros/projects/thiago-portfolio/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/thiagobarros/projects/thiago-portfolio/app/globals.css"
+    ]
+  }
+}
